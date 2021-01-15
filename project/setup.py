040dc8c086687a0176c9ea45c38e982496a1f946
Leonardo de Holanda Bonifácio <leohb2@gmail.com>
2021-01-15 18:52:02 -0300

    correção setup.py

--- project/setup.py
+++ project/setup.py
@@ -9,15 +9,10 @@ except FileNotFoundError:
     long_description = ''
 
 requirements = [
-    'fastapi == 0.60.1',
+    'fastapi == 0.61.1',
     'loguru == 0.5.3',
     'gunicorn == 20.0.4',
-    'uvicorn == 0.10.3',
-    'Jinja2 == 2.10.1',
-    'pymongo == 3.11.1',
-    'pymongo[srv] == 3.11.1',
-    'aiofiles == 0.5.0',
-    'SQLAlchemy == 1.3.19'
+    'uvicorn == 0.11.8',
 ]
 
 setup(
